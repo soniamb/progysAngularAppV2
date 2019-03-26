a08700661c5805e3c174f08a5c0be23c232fe58e
BabbaAmine <amine.babba@sesame.com.tn>
2019-03-27 00:33:57 +0100

    design ajout action

--- src/app/pages/Progys/actions/actions.component.ts
+++ src/app/pages/Progys/actions/actions.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { NbDateService } from '@nebular/theme';
 @Component({
   selector: 'ngx-alert',
   templateUrl: 'actions.component.html',
+
 })
 export class ActionsComponent {
+
+  constructor(protected dateService: NbDateService<Date>) {
+
+  }
 
 }
